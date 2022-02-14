a381e7bbc64064a5b389e4a5401483b240d66586
Tanuj Thakkar <tanujthakkar720@gmail.com>
2022-02-14 17:15:28 -0500

    Code cleanup

--- Code/Q2.py
+++ Code/Q2.py
@@ -20,9 +20,9 @@ import argparse
 import matplotlib.pyplot as plt
 
 
-def get_data_points():
+def get_data_points(VideoPath):
 
-    video = cv2.VideoCapture("./Videos/ball_video1.mp4") 
+    video = cv2.VideoCapture(VideoPath) 
 
     currentframe = 0
     frames = list() # Frames
@@ -30,6 +30,7 @@ def get_data_points():
     y_coords = list() # Y Coordinates of center of the ball in Image plane
     ret = True
 
+    # System of Linear Equations
     X = np.empty([0,3])
     Y = np.empty([0,1])
     a = np.empty([3,1])
@@ -39,15 +40,15 @@ def get_data_points():
         ret, frame = video.read()
         if(not ret):
             break
-        frame = cv2.resize(frame, dsize=None, fx=0.2, fy=0.2, interpolation=cv2.INTER_CUBIC)
-        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # frame = cv2.resize(frame, dsize=None, fx=0.2, fy=0.2, interpolation=cv2.INTER_CUBIC)
+        # gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-        # lower boundary RED color range values; Hue (0 - 10)
+        # Lower boundary RED color range values; Hue (0 - 10)
         lower1 = np.array([0, 100, 20])
         upper1 = np.array([10, 255, 255])
          
-        # upper boundary RED color range values; Hue (160 - 180)
+        # Upper boundary RED color range values; Hue (160 - 180)
         lower2 = np.array([160,100,20])
         upper2 = np.array([179,255,255])
          
@@ -59,44 +60,34 @@ def get_data_points():
         currentframe += 1
         frames.append(currentframe)
 
-        test = np.array(np.where(full_mask == 255))
+        PoI = np.array(np.where(full_mask == 255)) # Points of interest
 
-        # print(full_mask.shape)
-        # print(test.shape)
-        # print(test[0].argmax(), test[1].argmax())
-        # print(test[0].argmin(), test[1].argmin())
-        # print(test[0][test[0].argmax()], test[1][test[1].argmax()])
-        # print(test[0][test[0].argmin()], test[1][test[1].argmin()])
-
-        x = test[1][test[1].argmin()] + (test[1][test[1].argmax()] - test[1][test[1].argmin()])/2 # X/columns axis in Image plane, Y/rows in a matrix
-        y = test[0][test[0].argmin()] + (test[0][test[0].argmax()] - test[0][test[0].argmin()])/2 # Y/rows axis in Image plane, X/columns in a matrix
-
-        # print(x, y)
+        x = PoI[1][PoI[1].argmin()] + (PoI[1][PoI[1].argmax()] - PoI[1][PoI[1].argmin()])/2 # X/columns axis in Image plane, Y/rows in a matrix
+        y = PoI[0][PoI[0].argmin()] + (PoI[0][PoI[0].argmax()] - PoI[0][PoI[0].argmin()])/2 # Y/rows axis in Image plane, X/columns in a matrix
 
         full_mask = cv2.circle(frame, (int(x), int(y)), 2, (255,0,0), -1)
-        
+
+        y = abs(y - frame.shape[0]) # Transforming points to cartesian coordinates
+
         x_coords.append(x)
         y_coords.append(y)
 
-        # print(x**2, x, y)
         X = np.insert(X, len(X), np.array([x**2, x, 1]), axis=0)
         Y = np.insert(Y, len(Y), y, axis=0)
-        # print(X.shape, Y.shape)
 
-        cv2.imshow("Frame", frame)
-        cv2.imshow("Gray Frame", full_mask)
-        cv2.imshow("HSV Frame", hsv_frame)
-        cv2.waitKey(1)
+        # cv2.imshow("Frame", frame)
+        # cv2.imshow("Gray Frame", full_mask)
+        # cv2.imshow("HSV Frame", hsv_frame)
+        # cv2.waitKey(1)
 
-    a = np.matmul(np.matmul(np.linalg.inv(np.matmul(X.transpose(), X)), X.transpose()), Y)
-    print(a)
+    a = np.matmul(np.matmul(np.linalg.inv(np.matmul(X.transpose(), X)), X.transpose()), Y) # Solving the system of linear equations for a
 
     x_coords_sq = [x**2 for x in x_coords]
-    p = a[0]*x_coords_sq + a[1]*x_coords + a[2]
+    p = a[0]*x_coords_sq + a[1]*x_coords + a[2] # Generating points of the parabola
 
     fig = plt.figure()
     plt.scatter(x_coords, y_coords, linewidths=0.2)
-    plt.plot(x_coords, p)
+    plt.plot(x_coords, p, 'red')
     plt.show()
 
     video.release() 
@@ -104,8 +95,13 @@ def get_data_points():
 
 
 def main():
+    Parser = argparse.ArgumentParser()
+    Parser.add_argument('--VideoPath', type=str, default="./Videos/ball_video2.mp4", help='Select model')
+    
+    Args = Parser.parse_args()
+    VideoPath = Args.VideoPath
 
-    get_data_points()
+    get_data_points(VideoPath)
 
 
 if __name__ == '__main__':
