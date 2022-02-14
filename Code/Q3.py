903c47f2a4b84c77fc0f16b4de6e9becde8af915
Tanuj Thakkar <tanujthakkar720@gmail.com>
2022-02-14 17:16:03 -0500

    Added total_least_squares with issues

--- Code/Q3.py
+++ Code/Q3.py
@@ -43,6 +43,49 @@ def compute_covariance_matrix(data):
     return C
 
 
+def SVD(A):
+
+    # Computing U matrix
+    AAT = np.matmul(A, A.transpose())
+
+    eig_vals, eig_vecs = np.linalg.eig(AAT)
+    # print(len(eig_vals), len(eig_vecs))
+    eigs = {eig_vals[i]: eig_vecs[:,i] for i in range(len(eig_vals))}
+    eigs = {eig_val: eig_vec for eig_val, eig_vec in sorted(eigs.items(), reverse=True)}
+
+    U = np.array([eig_vec for eig_vec in eigs.values()])
+    # idx1 = np.flip(np.argsort(eig_vals))
+    # eig_vals = eig_vals[idx1]
+    # U = eig_vecs[:, idx1]
+    # print(U)
+    # print(U)
+
+    # Computing Sigma
+    Sigma = np.zeros([len(eig_vals), len(eig_vals)+1])
+    for i, eig_val in enumerate(eigs):
+        Sigma[i][i] = np.sqrt(eig_val)
+    # print(Sigma)
+
+    # Computing V matrix
+    ATA = np.matmul(A.transpose(), A)
+
+    eig_vals, eig_vecs = np.linalg.eig(ATA)
+    # print(len(eig_vals), len(eig_vecs))
+    eigs = {eig_vals[i]: eig_vecs[:,i] for i in range(len(eig_vals))}
+    eigs = {eig_val: eig_vec for eig_val, eig_vec in sorted(eigs.items(), reverse=True)}
+
+    V = np.array([eig_vec for eig_vec in eigs.values()])
+    # idx1 = np.flip(np.argsort(eig_vals))
+    # eig_vals = eig_vals[idx1]
+    # V = eig_vecs[:, idx1]
+    # print(V)
+
+    # print(U.shape, Sigma.shape, V.shape)
+    # print((V[-1]/V[-1, -1]).reshape((3,3)))
+
+    return U, Sigma, V
+
+
 def linear_least_squares(data):
 
     # Equation of line: y = mx + x
@@ -57,6 +100,31 @@ def linear_least_squares(data):
     return X, Y, a
 
 
+def total_least_squares(data):
+
+    # Equation of line: ax + by + c = 0
+    # print(data)
+    X = np.array([[data[0][i], data[1][i]] for i in range(len(data[0]))])
+    # a = np.empty([3,1])
+    # print(X)
+
+    U, Sigma, V = SVD(np.matmul(X.transpose(), X))
+    u, sigma, v = np.linalg.svd(X)
+    print(V)
+    print(v)
+    a = (V[:, -1]).reshape((2,1))
+    print(a)
+
+    # y = a[0]*data[0]+a[1]*data[1]
+    # x = a[0]*data[0]
+    y = (1 - 1*data[0])/1
+
+    fig = plt.figure()
+    plt.scatter(data[0], data[1], marker='.', linewidths=0.01)
+    plt.plot(data[0], y, 'red')
+    plt.show()
+
+
 def main():
 
     data = read_data("./Dataset/ENPM673_hw1_linear_regression_dataset.csv", ['age', 'charges']) # Get data from the CSV file
@@ -65,19 +133,23 @@ def main():
 
     eig_vals, eig_vecs = np.linalg.eig(cov_mat)
 
-    X, Y, a = linear_least_squares(data)
+    data[0] = (data[0] - data[0].min())/(data[0].max() - data[0].min())
+    data[1] = (data[1] - data[1].min())/(data[1].max() - data[1].min())
 
-    # x_coords_sq = [x**2 for x in data[0]]
-    p = a[0]*data[0] + a[1]
+    total_least_squares(data)
 
-    origin = [0, 0]
+    # X, Y, a = linear_least_squares(data)
 
-    fig = plt.figure()
-    plt.scatter(data[0], data[1], marker='.', linewidths=0.01)
-    plt.plot(data[0], p)
-    # plt.quiver(*origin, *eig_vecs[0], color=['r'], scale=21)
+    # p = a[0]*data[0] + a[1]
+
+    # origin = [0, 0]
+
+    # fig = plt.figure()
+    # plt.scatter(data[0], data[1], marker='.', linewidths=0.01)
+    # plt.plot(data[0], p, 'red')
+    # plt.quiver(*origin, *eig_vecs[:,0], color=['r'], scale=21)
     # plt.quiver(*origin, *eig_vecs[1], color=['b'], scale=21)
-    plt.show()
+    # plt.show()
 
 
 if __name__ == '__main__':
